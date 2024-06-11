35510726d9d7b66dd33689df4948de43c4ee9ce6
Alvaro Guerrero <alvaro.guerrero2212@gmail.com>
2024-06-11 09:59:22 -0300

    color

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ def algoritmo_genetico(cant_equipos:int,cant_adversarios:int,cant_generaciones:i
     effectiveness = efectividad()
     nueva_poblacion = poblacion(cant_equipos)
 
-    for _ in tqdm(range(cant_generaciones), desc="Generaciones", unit="gen"):
+    for _ in tqdm(range(cant_generaciones), desc="Generaciones", unit="gen",colour="blue"):
         adversarios = poblacion(cant_adversarios)
         aptitudes = evaluar_aptitud(nueva_poblacion, adversarios, effectiveness)
         seleccionados = seleccion_proporcional(aptitudes, cant_adversarios)
